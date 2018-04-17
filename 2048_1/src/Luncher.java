e6ac9b5e8cdf269ff8e6b9d8b227d482e3b452c8
jahirulRifat <35885439+jahirulRifat@users.noreply.github.com>
2018-04-17 16:40:40 +0600

    Added GameState class

--- 2048_1/src/Luncher.java
+++ 2048_1/src/Luncher.java
@@ -2,21 +2,55 @@
 public class Luncher {
 
 	public static void main(String[] args) {
+		
 
 		Game game =new Game();
-		
-		
-		for(int x=0;x<16;x++) {
-			game.addNumber();
-		}
-		game.printArray();
-		game.pushUp();
+//		
+//		
+//		for(int x=0;x<16;x++) {
+//			game.addNumber();
+//		}
 		game.printArray();
 		
-		
-		
-		
-
+//		game.pushDown();
+//		game.printArray();
+//		game.addNumber();
+//		game.printArray();
+//		
+//		game.pushUp();
+//		game.printArray();
+//		game.addNumber();
+//		game.printArray();
+//		
+//		game.pushRight();
+//		game.printArray();
+//		game.addNumber();
+//		game.printArray();
+//		
+//		game.pushLeft();
+//		game.printArray();
+//		game.addNumber();
+//		game.printArray();
+//		
+//		game.pushDown();
+//		game.printArray();
+//		game.addNumber();
+//		game.printArray();
+//		
+//		game.pushUp();
+//		game.printArray();
+//		game.addNumber();
+//		game.printArray();
+//		
+//		game.pushRight();
+//		game.printArray();
+//		game.addNumber();
+//		game.printArray();
+//		
+//		game.pushLeft();
+//		game.printArray();
+//		game.addNumber();
+//		game.printArray();
 	}
 
 }
